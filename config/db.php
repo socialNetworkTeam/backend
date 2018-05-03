970cb66c7f01f6ed4f49216f425e30a260eb10b1
olegbolden <oleg@bolden.ru>
2018-05-04 02:07:48 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,12 @@
 <?php
+$DB_HOST = getenv('DB_HOST');
+$DB_NAME = getenv('DB_NAME');
+$DB_USER = getenv('DB_USER');
+$DB_PASSWORD = getenv('DB_PASSWORD');
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=$DB_HOST;dbname=$DB_NAME',
+    'dsn' => "mysql:host=$DB_HOST;dbname=$DB_NAME",
     'username' => $DB_USER,
     'password' => $DB_PASSWORD,
     'charset' => 'utf8',
